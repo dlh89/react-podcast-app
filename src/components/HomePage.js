bf4f45af31aaa4ae0762e57dded9f2511b96577f
David Hide <d.hide@live.co.uk>
2018-05-06 18:34:53 +0100

    Added heroku postbuild script to package.json

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,7 @@ import SearchBar from '../components/SearchBar';
 const HomePage = () => (
     <div className="container">
         <SearchBar />
-        <h1>Podcast Info Home Page</h1>
+        <h1>Podsend</h1>
         <h3>Find out everything you wanted to know about your favourite podcasts.</h3>
     </div>
 )
